37210900a6b04544a1d7feb867efef56bee64c5e
Santiago Canovari <santiagocanovari@gmail.com>
2022-03-21 19:47:22 -0300

    App finished

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer');
+const { leerInput, inquirerMenu, pausa, listarLugares } = require('./helpers/inquirer');
 const Busquedas = require('./models/busquedas');
 
 
@@ -9,36 +9,47 @@ const main = async () => {
     let opt;
     do {
         opt = await inquirerMenu();
-        
+
         switch (opt) {
             case 1:
                 // Mostrar mensaje
-                const lugar = await leerInput('Ciudad: ');
-                console.log(lugar);
-                await busquedas.ciudad(lugar);
-                
-                // Seleccionar el lugar
+                const termino = await leerInput('Ciudad: ');
+                //Buscar los lugares
+                const lugares = await busquedas.ciudad(termino);
+                //Seleccionar el lugar
+                const id = await listarLugares(lugares);
+                if(id==='0') continue;
+
+                const lugarSel = lugares.find(l => l.id===id);
+
+                //Guardar en DB
+                busquedas.agregarHistorial(lugarSel.nombre);
 
                 //Clima
+                const clima = await busquedas.climaLugar(lugarSel.lat,lugarSel.lng);
 
                 //Mostrar resultados
 
                 console.log('\nInformación de la ciudad\n'.green);
-                console.log('Ciudad:',);
-                console.log('Latitud:',);
-                console.log('Longitud:',);
-                console.log('Temperatura:',);
-                console.log('Mínima:',);
-                console.log('Máxima:',);
+                console.log('Ciudad:', lugarSel.nombre);
+                console.log('Latitud:', lugarSel.lat);
+                console.log('Longitud:', lugarSel.lng);
+                console.log('Temperatura:',clima.temp);
+                console.log('Mínima:',clima.temp_min);
+                console.log('Máxima:',clima.temp_max);
+                console.log('Clima:',clima.description)
                 break;
             case 2:
-                console.log(`Eligio la opción${opt}`)
+                busquedas.historialCapitalizado.forEach((lugar,i)=>{
+                    const idx=`${i+1}.`.green
+                    console.log(`${idx} ${lugar}`)
+                })
                 break;
             case 0:
                 console.log(`Eligio la opción${opt}`)
                 break;
         }
-        
+
 
         if (opt !== 0) await pausa();
 
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,19 +1,38 @@
-const axios = require('axios');
+const fs = require('fs')
 
+const axios = require('axios');
 console.log(process.argv);
 
 class Busquedas {
-    historial = ['Tegucigalpa', 'Madrid', 'Buenos Aires'];
+    historial = [];
+    dbPath = './db/database.json';
 
     constructor() {
-        //TODO:leer DB si existe
+        this.leerDB();
     }
 
-    get paramsMapbox(){
-        return{
+    get historialCapitalizado(){
+        return this.historial.map(lugar =>{
+            let palabras =  lugar.split(' ');
+            palabras = palabras.map(p => p[0].toUpperCase()+p.substring(1));
+
+            return palabras.join(' ');
+        })
+    }
+
+    get paramsMapbox() {
+        return {
             'access_token': process.env.MAPBOX_KEY,
             'language': 'es',
             'limit': 5
+        }
+    }
+
+    get paramsOpenweather() {
+        return {
+            appid: process.env.OPENWEATHER_KEY,
+            lang: 'es',
+            units: 'metric'
         }
     }
 
@@ -25,8 +44,13 @@ class Busquedas {
                 params: this.paramsMapbox
             });
             const resp = await instance.get(`${lugar}.json`);
-            console.log(resp.data)
-            return [];
+            return resp.data.features.map(lugar => ({
+                id: lugar.id,
+                nombre: lugar.place_name,
+                lng: lugar.center[0],
+                lat: lugar.center[1],
+
+            }));
 
         } catch (error) {
             console.log(`No se encontró ninguna información`.red);
@@ -35,6 +59,58 @@ class Busquedas {
         }
 
         //Retonarlos lugares quecoincidancon lo solicitado
+    }
+
+    async climaLugar(lat, lon) {
+        try {
+            //Instance
+            const instance = axios.create({
+                baseURL: 'https://api.openweathermap.org/data/2.5/weather',
+                params: { ...this.paramsOpenweather, lat, lon }
+            });
+            const resp = await instance.get();
+            const { weather, main } = resp.data;
+
+            return {
+                description: weather[0].description,
+                temp_min: main.temp_min,
+                temp_max: main.temp_max,
+                temp: main.temp
+            }
+        } catch (error) {
+            console.log("No se encontró temperatira para la ciudad".red)
+            console.log(error);
+        }
+    }
+
+
+    agregarHistorial(lugar = '') {
+        //TODO: prevenir duplicados
+        if (this.historial.includes(lugar.toLowerCase())) {
+            return;
+        }
+        this.historial=this.historial.splice(0,4);
+        this.historial.unshift(lugar.toLowerCase());
+
+        //Grabar en DB
+        this.guardarDB()
+    }
+
+    guardarDB() {
+        const payload = {
+            historial: this.historial
+        }
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+    }
+
+    leerDB() {
+        if(!fs.existsSync(this.dbPath)) return;
+
+        const info = fs.readFileSync(this.dbPath,{encoding:'utf-8'});
+        const data = JSON.parse(info);
+
+        this.historial = data.historial;
+
     }
 
 
